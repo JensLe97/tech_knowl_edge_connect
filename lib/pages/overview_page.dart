b05c76ba30d8e46c16e59688089667ff933b1901
JensLe97 <lemkejens@yahoo.de>
2024-02-04 22:01:20 +0100

    Add Safe area

--- lib/pages/overview_page.dart
+++ lib/pages/overview_page.dart
@@ -33,21 +33,24 @@ class _OverviewPageState extends State<OverviewPage> {
       body: IndexedStack(children: [
         _tabs.elementAt(_currentIndex),
       ]),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        currentIndex: _currentIndex,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Theme.of(context).textTheme.displayLarge!.color,
-        unselectedItemColor: Theme.of(context).textTheme.bodyLarge!.color,
-        onTap: _onTap,
-        items: const [
-          BottomNavigationBarItem(
-            label: 'Home',
-            icon: Icon(Icons.home),
-          ),
-          BottomNavigationBarItem(label: 'Suche', icon: Icon(Icons.search)),
-          BottomNavigationBarItem(label: 'Profil', icon: Icon(Icons.person)),
-        ],
+      bottomNavigationBar: SafeArea(
+        bottom: false,
+        child: BottomNavigationBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          currentIndex: _currentIndex,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Theme.of(context).textTheme.displayLarge!.color,
+          unselectedItemColor: Theme.of(context).textTheme.bodyLarge!.color,
+          onTap: _onTap,
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(label: 'Suche', icon: Icon(Icons.search)),
+            BottomNavigationBarItem(label: 'Profil', icon: Icon(Icons.person)),
+          ],
+        ),
       ),
     );
   }

c477d82c2bf6e2af155fff71d97f5bd1831da880
JensLe97 <lemkejens@yahoo.de>
2024-02-04 22:23:02 +0100

    padding

--- lib/pages/overview_page.dart
+++ lib/pages/overview_page.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tech_knowl_edge_connect/pages/home/home_page.dart';
 import 'package:tech_knowl_edge_connect/pages/profile/profile_page.dart';
@@ -33,23 +34,28 @@ class _OverviewPageState extends State<OverviewPage> {
       body: IndexedStack(children: [
         _tabs.elementAt(_currentIndex),
       ]),
-      bottomNavigationBar: SafeArea(
-        bottom: false,
-        child: BottomNavigationBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          currentIndex: _currentIndex,
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: Theme.of(context).textTheme.displayLarge!.color,
-          unselectedItemColor: Theme.of(context).textTheme.bodyLarge!.color,
-          onTap: _onTap,
-          items: const [
-            BottomNavigationBarItem(
-              label: 'Home',
-              icon: Icon(Icons.home),
-            ),
-            BottomNavigationBarItem(label: 'Suche', icon: Icon(Icons.search)),
-            BottomNavigationBarItem(label: 'Profil', icon: Icon(Icons.person)),
-          ],
+      bottomNavigationBar: Container(
+        color: Theme.of(context).colorScheme.inversePrimary,
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: kIsWeb ? 25 : 0),
+          child: BottomNavigationBar(
+            elevation: 0,
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            currentIndex: _currentIndex,
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Theme.of(context).textTheme.displayLarge!.color,
+            unselectedItemColor: Theme.of(context).textTheme.bodyLarge!.color,
+            onTap: _onTap,
+            items: const [
+              BottomNavigationBarItem(
+                label: 'Home',
+                icon: Icon(Icons.home),
+              ),
+              BottomNavigationBarItem(label: 'Suche', icon: Icon(Icons.search)),
+              BottomNavigationBarItem(
+                  label: 'Profil', icon: Icon(Icons.person)),
+            ],
+          ),
         ),
       ),
     );

e70641c260bb6a183f5b99d690c5792502aa0601
JensLe97 <lemkejens@yahoo.de>
2024-02-04 23:21:18 +0100

    update colors

--- lib/main.dart
+++ lib/main.dart
@@ -66,11 +66,8 @@ class TechKnowlEdgeConnect extends StatelessWidget {
             elevation: 0,
           ),
           bottomSheetTheme: const BottomSheetThemeData(
-            dragHandleColor: Colors.transparent,
-            modalBarrierColor: Colors.transparent,
-            surfaceTintColor: Colors.transparent,
             shadowColor: Colors.transparent,
-            modalBackgroundColor: Colors.transparent,
+            surfaceTintColor: Colors.transparent,
             backgroundColor: Colors.transparent,
           )),
       home: const AuthPage(),

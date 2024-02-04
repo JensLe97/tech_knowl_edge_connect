5d37c29bc47afe4513b49f161a7049af8e86eb93
JensLe97 <lemkejens@yahoo.de>
2024-02-04 20:09:52 +0100

    Reverse

--- lib/pages/overview_page.dart
+++ lib/pages/overview_page.dart
@@ -35,48 +35,22 @@ class _OverviewPageState extends State<OverviewPage> {
       body: IndexedStack(children: [
         _tabs.elementAt(_currentIndex),
       ]),
-      bottomNavigationBar: kIsWeb
-          ? WebSafeAreaInsets(
-              child: BottomNavigationBar(
-                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                currentIndex: _currentIndex,
-                type: BottomNavigationBarType.fixed,
-                selectedItemColor:
-                    Theme.of(context).textTheme.displayLarge!.color,
-                unselectedItemColor:
-                    Theme.of(context).textTheme.bodyLarge!.color,
-                onTap: _onTap,
-                items: const [
-                  BottomNavigationBarItem(
-                    label: 'Home',
-                    icon: Icon(Icons.home),
-                  ),
-                  BottomNavigationBarItem(
-                      label: 'Suche', icon: Icon(Icons.search)),
-                  BottomNavigationBarItem(
-                      label: 'Profil', icon: Icon(Icons.person)),
-                ],
-              ),
-            )
-          : BottomNavigationBar(
-              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-              currentIndex: _currentIndex,
-              type: BottomNavigationBarType.fixed,
-              selectedItemColor:
-                  Theme.of(context).textTheme.displayLarge!.color,
-              unselectedItemColor: Theme.of(context).textTheme.bodyLarge!.color,
-              onTap: _onTap,
-              items: const [
-                BottomNavigationBarItem(
-                  label: 'Home',
-                  icon: Icon(Icons.home),
-                ),
-                BottomNavigationBarItem(
-                    label: 'Suche', icon: Icon(Icons.search)),
-                BottomNavigationBarItem(
-                    label: 'Profil', icon: Icon(Icons.person)),
-              ],
-            ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Theme.of(context).textTheme.displayLarge!.color,
+        unselectedItemColor: Theme.of(context).textTheme.bodyLarge!.color,
+        onTap: _onTap,
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(label: 'Suche', icon: Icon(Icons.search)),
+          BottomNavigationBarItem(label: 'Profil', icon: Icon(Icons.person)),
+        ],
+      ),
     );
   }
 

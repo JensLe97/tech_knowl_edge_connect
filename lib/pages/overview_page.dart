01a4e05c1ce537b7a896be8ec6026c04f9d04036
JensLe97 <lemkejens@yahoo.de>
2024-02-05 23:37:34 +0100

    Remove padding around navbar

--- lib/pages/overview_page.dart
+++ lib/pages/overview_page.dart
@@ -36,26 +36,22 @@ class _OverviewPageState extends State<OverviewPage> {
       ]),
       bottomNavigationBar: Container(
         color: Theme.of(context).colorScheme.inversePrimary,
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: kIsWeb ? 25 : 0),
-          child: BottomNavigationBar(
-            elevation: 0,
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            currentIndex: _currentIndex,
-            type: BottomNavigationBarType.fixed,
-            selectedItemColor: Theme.of(context).textTheme.displayLarge!.color,
-            unselectedItemColor: Theme.of(context).textTheme.bodyLarge!.color,
-            onTap: _onTap,
-            items: const [
-              BottomNavigationBarItem(
-                label: 'Home',
-                icon: Icon(Icons.home),
-              ),
-              BottomNavigationBarItem(label: 'Suche', icon: Icon(Icons.search)),
-              BottomNavigationBarItem(
-                  label: 'Profil', icon: Icon(Icons.person)),
-            ],
-          ),
+        child: BottomNavigationBar(
+          elevation: 0,
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          currentIndex: _currentIndex,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Theme.of(context).textTheme.displayLarge!.color,
+          unselectedItemColor: Theme.of(context).textTheme.bodyLarge!.color,
+          onTap: _onTap,
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(label: 'Suche', icon: Icon(Icons.search)),
+            BottomNavigationBarItem(label: 'Profil', icon: Icon(Icons.person)),
+          ],
         ),
       ),
     );

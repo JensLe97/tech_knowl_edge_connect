c906cc57f6b89739206b5cce97e2f81205c487f8
JensLe97 <mail@lemkejens.de>
2026-03-15 17:25:54 +0100

    chore: remove unused import of font_awesome_flutter from app_test.dart

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:tech_knowl_edge_connect/models/content/subject.dart';
 import 'package:tech_knowl_edge_connect/components/tiles/subject_tile.dart';
